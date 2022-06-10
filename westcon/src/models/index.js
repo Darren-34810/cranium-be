7b9d8c48b72035c57902f4d4779104395377ba25
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2022-06-10 14:50:35 +0700

    express typescript

--- westcon/src/models/index.js
+++ westcon/src/models/index.js
@@ -1,17 +1,12 @@
 'use strict';
 
-import { readdirSync } from 'fs';
-import { basename as _basename, join } from 'path';
-import Sequelize, { DataTypes } from 'sequelize';
-const basename = _basename(__filename);
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
-console.log(config);
-console.log(__dirname);
-console.log(__basename);
-console.log(__filename);
 
 let sequelize;
 if (config.use_env_variable) {
@@ -20,12 +15,13 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-readdirSync(__dirname)
+fs
+  .readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = require(join(__dirname, file))(sequelize, DataTypes);
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
@@ -38,4 +34,4 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-export default db;
+module.exports = db;
